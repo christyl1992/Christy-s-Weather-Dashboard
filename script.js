4d9e60c7b2c6cdf066a7a4a4fe9f63f457240c69
Christy Llewellyn <christy.elizabeth@hotmail.co.uk>
2024-01-30 16:41:30 +0000

    Created an array of all the weather data

--- script.js
+++ script.js
@@ -7,22 +7,12 @@ var city= "london";
 
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-
-// fetch(queryURL)
-
-
-// // Add your own API key between the ""
-// var APIKey = "1f3c2c140755fa984ae37713c1ff8a71";
-
-// // Here we are building the URL we need to query the database
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + 1f3c2c140755fa984ae37713c1ff8a71;
+var listweatherdata = []
 
 
 function isNoon(entry) {
     return entry.dt_txt.endsWith(" 12:00:00");
 }
-
-
 
 // We then created an Fetch call
 fetch(queryURL)
@@ -34,15 +24,17 @@ fetch(queryURL)
     // Create CODE HERE to Log the queryURL
     console.log (queryURL);
     console.log (data); 
-    var name = (data.name);
-    var date = (data.dt);
-    var temp = (data.main.temp);
-    var humidity = (data.main.humidity);
-    var wind = (data.wind.speed);
-    var lon = (data.coord.lon);
-    var lat = (data.coord.lat);
-    var string_to_print = name+" "+date+" "+temp+" "+humidity+" "+wind
-    console.log(string_to_print);
+    const name = (data.name);
+    const date = (data.dt);
+    const temp = (data.main.temp);
+    const humidity = (data.main.humidity);
+    const wind = (data.wind.speed);
+    const lon = (data.coord.lon);
+    const lat = (data.coord.lat);
+    // var string_to_print = name+" "+date+" "+temp+" "+humidity+" "+wind
+    // console.log(string_to_print);
+    const commonData = {temp:temp, humidity:humidity, date:date}
+    listweatherdata.push(commonData)
 
      //5 days
     var second_queryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
@@ -54,9 +46,28 @@ fetch(queryURL)
       .then(function (data) {
         console.log(data); 
         // Extract entries with time at 12:00 PM
-        const noonData = Object.values(data.list).filter(isNoon);
-        console.log(noonData); 
+        const noonData = {}
+        for (const key in data.list){
+            // console.log(key)
+            const entry = data.list[key];
+ 
+            if (isNoon(entry)) {
+                
+                const dateFive = (entry.dt);
+                const temperatureFive = (entry.main.temp);
+                const humidityFive = (entry.main.humidity);
+
+                const commonData = {temp:temperatureFive, humidity:humidityFive, date:dateFive}
+                // console.log(commonData)
+                listweatherdata.push(commonData)
+            }
+        }
+        // Object.values(data.list).filter(isNoon);
+        // console.log(noonData); 
     // Function to check if the time in the dt_txt property is 12:00 PM
 
+    console.log(listweatherdata[0].temp)
+    console.log(listweatherdata[1].humidity)
+    // console.log(listweatherdata)
  })
   
